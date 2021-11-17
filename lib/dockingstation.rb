21734a8719a4c617d3300b92d7869e186eb2c217
Kimberley Morgan <kimberley.morgan135@gmail.com>
2021-11-17 15:40:21 +0000

    Introduced constant

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -5,22 +5,25 @@ class DockingStation
         @station = []
     end
 
-    def empty?
-        @station.length == 0
-    end
 
     def release_bike
         fail "No bikes available" if empty?
         @station.pop()
     end
     
-    def full?
-        @station.length >= 20
-    end
 
     def dock(bike)
         fail "Station full" if full?
         @station.push(bike)
     end
-    private :empty?, :full?
+    
+    private
+    
+    def full?
+        @station.length >= 20
+    end
+
+    def empty?
+        @station.length == 0
+    end
 end
